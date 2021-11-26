c65f477c9592e621a6447ed3382f8e6ea4cf69db
Tumeka <65959122+Tumekab@users.noreply.github.com>
2021-11-26 14:06:19 +0000

    started adding audioplayer css

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {BsArrowLeftShort} from "react-icons/bs";
+import {BsArrowRightShort} from "react-icons/bs";
+import {FaPlay} from "react-icons/fa";
+import {FaPause} from "react-icons/fa";
 
 const AudioPlayer = () => {
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const togglePlayPause = () => {
+        setIsPlaying(!isPlaying)
+    }
+
     return (
         <div>
-            <button>Previous Song</button>
-            <button>Play/Pause</button>
-            <button>Next Song</button>
+            <button className="forwardBackward">
+                <BsArrowLeftShort />
+            </button>
+
+            <button onClick={togglePlayPause} className="playPause">
+                {isPlaying ? <FaPause /> : <FaPlay />}
+            </button>
+
+            <button className="forwardBackward">
+                <BsArrowRightShort />
+            </button>
 
             {/* current time */}
             <div>0:00</div>
@@ -17,6 +35,9 @@ const AudioPlayer = () => {
 
             {/* duration */}
             <div>1:49</div>
+
+            {/* shuffle */}
+            {/* repeat */}
         </div>
     )
 }
