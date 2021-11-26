6537c013a55ba0dcc3f60e4c169097296332b93b
Tumeka <65959122+Tumekab@users.noreply.github.com>
2021-11-26 13:07:00 +0000

    aded header and page title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './styles/main.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div >
-     <p>hello world</p>
+     <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => (
+    <>
+        <header>
+            <h1> K.K Slider's Music Library</h1>
+        </header>
+    </>
+)
+
+export default Header;
