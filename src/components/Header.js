35c44dab9733f2fc0d6dad4f450ed82dce268deb
Tumeka <65959122+Tumekab@users.noreply.github.com>
2021-11-27 17:26:42 +0000

    remembered that @font-face existed
    q

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => (
     <>
         <header>
-            <h1> K.K Slider's Music Library</h1>
+            <h1> K.K Slider's Music Library </h1>
         </header>
     </>
 )
